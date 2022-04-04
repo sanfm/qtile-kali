2e52981ad9a7b23b256a9684ed020eea58ec5864
sanfm <88844975+sanfm@users.noreply.github.com>
2022-04-04 05:14:45 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Make directories
 mkdir -p ~/.config/qtile
 mkdir -p ~/.cocnfig/rofi
+mkdir -p ~/Pictures/wallpapers
 
+# Copy config files
 cp config/qtile/* ~/.config/qtile/
 cp config/rofi/* ~/.cocnfig/rofi/
 
+# Copy scripts to the $PATH
 cp bin/* ~/.local/bin/
+
+# Copy wallpapers
+cp wallpapers/* ~/Pictures/wallpapers/
+
+# Set a background for betterlockscreen
+betterlockscreen -u ~/Pictures/wallpapers/wp-2.jpg
